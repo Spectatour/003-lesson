24d92fbdab426c71059714f90bb80174d4cecd31
Spectatour <ekalpokas@gmail.com>
2023-02-20 16:55:24 +0200

    react styling

--- src/Components/003/Bebras.jsx
+++ src/Components/003/Bebras.jsx
@@ -1,9 +1,31 @@
+import rand from "../Functions/rand";
 function Bebras() {
 
+    const fun = () => {
+        let a = '';
+        for (let i = 0; i <= 9; i++) {
+            a = a + i;
+
+        }
+        return a;
+    };
+
     return (
-        <h1>
-            <i>Bebras</i>
-        </h1>
+        <>
+            <h1>
+                <i style={{
+                    color: "skyblue",
+                    backgroundColor: rand(0, 1) ? 'coral' : 'pink'
+                    
+                    }}>Bebras</i>
+            </h1>
+            <h2 className="red">
+                Barsuku yra
+                {
+                    fun()
+                }
+            </h2>
+        </>
     );
 
 }
