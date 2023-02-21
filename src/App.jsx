8694d255c556857fcef58453e89dfeada70e6d90
Spectatour <ekalpokas@gmail.com>
2023-02-21 11:34:05 +0200

    Wish

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { useState } from 'react';
 
 const animals = [
-    {name: 'Racoon', color: 'skyblue', big: false},
-    {name: 'Fox', color: 'brown', big: false},
-    {name: 'Moose', color: 'yellow', big: true},
-    {name: 'Wolf', color: 'gray', big: false},
+    { name: 'Racoon', color: 'skyblue', big: false },
+    { name: 'Fox', color: 'brown', big: false },
+    { name: 'Moose', color: 'yellow', big: true },
+    { name: 'Wolf', color: 'gray', big: false },
 ];
 
 
@@ -15,9 +15,12 @@ function App() {
     const [h2Color, setH2Color] = useState('crimson');
     const [count, setCount] = useState(1);
 
+    const [wish, setWish] = useState('');
+    const [size, setSize] = useState('');
+
     const clickIt = () => {
         console.log('Hello World')
-        setH2Color(c => c ==='crimson' ? 'yellow': 'crimson');
+        setH2Color(c => c === 'crimson' ? 'yellow' : 'crimson');
     };
 
     const add1 = () => {
@@ -28,32 +31,45 @@ function App() {
         setCount(c => c - 1)
     };
 
-  return (
-    <div className="App">
-      <header className="App-header">
+    const doWish = e => {
+        setWish(e.target.value);
+    };
 
-        <div className="card">
-        <div className="card-header">
-            <h2 style={{color: h2Color}}>Forest Book (page {count})</h2>
+    const doWishSize = e => {
+        setSize(e.target.value);
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="card">
+                    <div className="card-header">
+                        <h2 style={{ color: h2Color }}>Forest Book (page {count})</h2>
+                    </div>
+                    <ul className="list-group list-group-flush">
+                        {
+                            animals.map((a, i) =>
+                                <li key={i} className={'list-group-item' + (a.big ? ' big' : '')}
+                                    style={{ color: a.color }}>
+                                    {a.name}
+                                </li>)
+                        }
+                    </ul>
+                    <div className="m-3">
+                        <label className="form-label">Enter your wish</label>
+                        <input type="text" className="form-control" onChange={doWish} value={wish} />
+                    </div>
+                    <div className="m-3">
+                        <label className="form-label">How Big <i>{size}</i></label>
+                        <input type="range" className="form-control" min="0" max="10" onChange={doWishSize} value={size} />
+                    </div>
+                </div>
+
+                <button type="button" className="btn btn-outline-warning mt-4" onClick={clickIt}>CLICK</button>
+                <button type="button" className="btn btn-outline-danger mt-4" onClick={add1}>+ 1</button>
+            </header>
         </div>
-        <ul className="list-group list-group-flush">
-            {
-                animals.map((a, i) => 
-                <li key={i} className={"list-group-item" + (a.big ? ' big ' : '')} 
-                style={{color:a.color}}>
-                    {a.name}
-                </li>)
-            }
-        </ul>
-        </div>
-        <div>
-            <button type="button" className="btn btn-outline-warning mt-4"onClick={clickIt}>CLICK</button>
-            <button type="button" className="btn btn-outline-danger mt-4"onClick={add1}>+1</button>
-            <button type="button" className="btn btn-outline-danger mt-4"onClick={add2}>-1</button>
-        </div>
-      </header>
-    </div>
-  );
+    );
 }
 
 export default App;
