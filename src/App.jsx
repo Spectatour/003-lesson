c8b677c6b804d98187bb9b5f97160684147bac01
Spectatour <ekalpokas@gmail.com>
2023-02-23 13:08:06 +0200

    Crud 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
 import './App.scss';
 import Create from './Components/006/Create';
+import { create } from './Components/Functions/localStorage';
 
-
+const KEY = 'whishList';
 
 
 function App() {
 
+    const [createData, setCreateData] = useState(null);
+
+    useEffect(() => {
+        if (null === createData) {
+            return;
+        }
+        create(KEY, createData);
+        // setLastRefresh(Date.now())
+    }, [createData]);
 
     return (
         <div className="container">
