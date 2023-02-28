556c4eb3537927ceb4bb8f8db236b7a9437598e6
Spectatour <ekalpokas@gmail.com>
2023-02-28 11:34:48 +0200

    PHP 001

--- src/Components/008/index.php
+++ src/Components/008/index.php
@@ -1,2 +1,47 @@
 <?php
-    echo 'Labas';
+
+    $labas = 5;
+
+    // echo $labas++;
+    echo '<br>';
+    echo $labas;
+
+    echo $labas++ + ++$labas;
+
+    $pirmas = 'bla bla';
+    $antras = 'ku ku ku';
+    $trecias = $pirmas . $antras;
+
+    echo '<br>';
+    echo $trecias;
+
+    echo '<br>';
+
+    $trecias2 = null;
+
+    var_dump(isset($trecias2));
+
+    echo '<br>';
+
+    var_dump($trecias2===Null);
+
+    $vienas = 'a';
+    $a = 'b';
+    $b = 'c';
+
+    echo '<br>';
+    echo $$$vienas;
+
+    echo '<br>';
+    echo '<pre>';
+     
+    print_r([1,5,8,7,9,32,8,7,[45,2,3,7],3,8,7,6]);
+
+    echo '<br>';
+
+    $pirmas = 'bla bla';
+    $antras = "ku ku";
+
+    echo '<br>';echo '<br>';
+    echo $antras;
+
